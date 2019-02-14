b710f10b998122dada131b34a9e5ef1fe785d39f
Manuel Spagnolo <spagnolo.manu@gmail.com>
2019-02-14 23:42:54 +0100

    feat: add heading component

---
+++ src/components/Heading/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from 'styled-components'
+
+import {size} from "~style/variables";
+import {baseLineHeight, primaryFont, secondaryFont} from "~style/typography";
+
+const H1 = styled.h1({
+  fontFamily: primaryFont.fontFamily,
+  lineHeight: baseLineHeight,
+  fontWeight: 900,
+  letterSpacing: 0,
+  fontSize: size(3)
+})
+
+const H2 = styled.h2({
+  fontFamily: secondaryFont.fontFamily,
+  lineHeight: baseLineHeight,
+  fontWeight: 400,
+  letterSpacing: '0.15px',
+  fontSize: size(2)
+})
+
+const headingMap = {
+  1: H1,
+  2: H2
+}
+
+export const Heading = ({level = 1, children}) => {
+  return React.createElement(headingMap[level], {children})
+}
