16db249d3ac9a435694237682750d960bbd69b34
Manuel Spagnolo <spagnolo.manu@gmail.com>
2019-02-16 18:43:59 +0100

    feat: add grid breakpoints

--- src/style/variables.js
+++ src/style/variables.js
@@ -1,3 +1,14 @@
-const BASE_SIZE = 8
+const BASE_SIZE = 8;
+const BREAKPOINT = {
+  XS: 480,
+  SM: 768,
+  MD: 980,
+  LG: 1184,
+  XL: 1366,
+  MG: 1920
+};
 
-export const size = (multiplier = 1) => `${BASE_SIZE * multiplier}px`
+export const size = (multiplier = 1) => `${BASE_SIZE * multiplier}px`;
+
+export const breakpoint = (size = "SM") =>
+  `${BREAKPOINT[String(size).toUpperCase()]}px`;
