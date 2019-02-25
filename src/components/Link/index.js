5e76857fb7320857e83a7cf5a43d26ab616fd369
Manuel Spagnolo <spagnolo.manu@gmail.com>
2019-02-25 21:41:07 +0100

    feat: add a11y friendly onclick feature for button

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,5 +1,25 @@
-import { Link } from 'gatsby'
+import React from 'react'
+import { Link as GatsbyLink } from 'gatsby'
 import styled from 'styled-components'
+
+const UnstyledButton = styled.button`
+  background:none !important;
+  border:none; 
+  padding:0!important;
+  cursor: pointer;
+  
+  &:focus {outline:0;}
+`
+
+const Link = ({ href, onClick, children, ...rest }) => {
+  if (href) {
+    return React.createElement('a', { children, href, ...rest })
+  } else if (onClick) {
+    return React.createElement(UnstyledButton, { children, onClick, ...rest })
+  }
+
+  return React.createElement(GatsbyLink, { children, ...rest })
+}
 
 export default styled(Link)`
   text-decoration: none;
