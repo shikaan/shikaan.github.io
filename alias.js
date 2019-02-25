987db7615e2dc5c3e746442b87829effc988dc43
Manuel Spagnolo <spagnolo.manu@gmail.com>
2019-02-25 21:39:07 +0100

    chore: add alias to improve webstorm code completion

---
+++ alias.js
@@ -0,0 +1,16 @@
+// WORKAROUND TO MAKE WEBSTORM/PHPSTORM UNDERSTAND ALIASES
+// https://youtrack.jetbrains.com/issue/WEB-22717#focus=streamItem-27-1558931-0-0
+
+/* eslint-disable */
+
+System.config({
+  'paths': {
+    '~/*': './src/*',
+    '~components/*': './src/components/*',
+    '~pages/*': './src/pages/*',
+    '~templates/*': './src/templates/*',
+    '~theme/*': './src/theme/*',
+  }
+})
+
+/* eslint-enable */
