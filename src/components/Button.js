4edb27fe800ce9d16ac68207d35e891ab0ddd798
Manuel Spagnolo <spagnolo.manu@gmail.com>
2019-02-27 23:10:16 +0100

    feat: create button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-export default class Button extends React.Component {
-  render() {
-    const { children, onClick } = this.props;
+const StyledButton = styled.button(({theme, context}) => `
+  font-family: ${theme.typography.primaryFont.fontFamily};
+  font-size: ${theme.typography.baseFontSize.multiply(0.875)};
+  font-weight: 700;
+  color: ${theme.color.white};
+  
+  background-color: ${theme.color[context]};
+  border:none;
+  padding: ${theme.typography.baseFontSize.multiply(.5)} ${theme.typography.baseFontSize.multiply(1.5)};
+  border-radius: 2px;
+`)
 
-    return <button onClick={onClick}>{children}</button>;
-  }
-}
+const Button = (props) => <StyledButton {...props} />
+
+Button.propTypes = {
+  context: PropTypes.oneOf(['coffee'])
+};
+
+export default Button;
--- src/pages/ReadArticle/Newsletter.js
+++ src/pages/ReadArticle/Newsletter.js
@@ -2,11 +2,20 @@ import React, {Component} from 'react';
 import styled from "styled-components";
 
 import {NEWSLETTER_WEBHOOK_ADDRESS} from "~/constants";
+
 import Button from '~components/Button'
+import Heading from '~components/Heading'
+import Form from '~components/Form'
+import Input from '~components/Input'
 
-const Section = styled.section`
+const Section = styled.section(({theme}) => `
+  text-align: center;
+  padding: ${theme.templateVariables.horizontalPadding}
+`)
 
-`
+const NewsletterHeading = styled(Heading)(({theme}) => `
+  padding-bottom: ${theme.templateVariables.verticalPadding};
+`)
 
 class Newsletter extends Component {
   inputRef = React.createRef()
@@ -25,6 +34,7 @@ class Newsletter extends Component {
 
   handleSubscribe = (e) => {
     e.preventDefault()
+
     const isEmailValid = this.inputRef.current.reportValidity()
 
     if (isEmailValid) {
@@ -33,11 +43,24 @@ class Newsletter extends Component {
   }
 
   render() {
+    const {content} = this.props
+
     return (
       <Section>
+        <NewsletterHeading level={2} sub={content.newsletter.subTitle}>
+          {content.newsletter.title}
+        </NewsletterHeading>
         <form onSubmit={this.handleSubscribe}>
-          <input ref={this.inputRef} type="email" value={this.state.email} onChange={this.handleEmailChange}/>
-          <Button type="submit">Subscribe</Button>
+          <Input
+            inputRef={this.inputRef}
+            type="email"
+            placeholder={content.newsletter.inputPlaceholder}
+            value={this.state.email}
+            onChange={this.handleEmailChange}/>
+
+          <Button context="coffee" type="submit">
+            {content.newsletter.ctaLabel}
+          </Button>
         </form>
       </Section>
     );
