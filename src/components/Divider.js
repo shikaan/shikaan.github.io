aafb2ea7b8fdb098fae13a617e62e870ebde42b3
Manuel Spagnolo <spagnolo.manu@gmail.com>
2019-03-02 18:57:18 +0100

    fix: divider should not autospace

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components'
-import { Size } from '~theme'
 
 export default styled.hr(({ theme }) => `
   height: 1px;
-  margin: 0 ${new Size(2)}
   color: ${theme.color.lightGrey};
 `)
