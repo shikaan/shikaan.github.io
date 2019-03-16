13b092b7e9e0156a52bca8db69e772f3e3fac499
Manuel Spagnolo <spagnolo.manu@gmail.com>
2019-03-16 19:26:07 +0100

    chore: remove some useless bureaucracy from Image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,3 @@
-import React from 'react'
 import Img from 'gatsby-image'
 
-const Image = ({ src, alt, ...rest }) => {
-  return src
-    ? <img src={src} alt={alt} {...rest} />
-    : <Img {...rest} />
-}
-
-export default Image
+export default Img
