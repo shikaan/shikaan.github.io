657051ba0c7e644d58b38cc8b13b6e245e23c936
Manuel Spagnolo <spagnolo.manu@gmail.com>
2019-04-13 20:31:28 +0200

    feat: create add method to sum sizes

--- src/theme/size.js
+++ src/theme/size.js
@@ -2,19 +2,30 @@ export default class Size {
   static BASE_SIZE = 8;
   static BASE_UNIT = "px";
 
-  constructor (rank) {
+  constructor(rank) {
     this._rank = Number.parseFloat(rank);
   }
 
-  toString () {
+  toString() {
     return `${this._rank * Size.BASE_SIZE}${Size.BASE_UNIT}`;
   }
 
-  get px () {
+  get px() {
     return this._rank * Size.BASE_SIZE;
   }
 
-  multiply (multiplier) {
+  multiply(multiplier) {
     return new Size(this._rank * multiplier);
+  }
+
+  /**
+   * @param {Size} other
+   */
+  add(other) {
+    if (!other || !(other instanceof Size)) {
+      throw new TypeError(`Addend has to be of type Size, ${other.name || typeof other} given`);
+    }
+
+    return new Size(this._rank + other._rank);
   }
 }
