98686e1555e0e6dd0a6f7ba696a85212b51c4cda
Manuel Spagnolo <spagnolo.manu@gmail.com>
2019-04-13 20:44:44 +0200

    feat: add google analytics support

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: process.env.GA_ID
       }
     },
     "gatsby-plugin-feed",
