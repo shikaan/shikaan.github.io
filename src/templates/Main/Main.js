be22b7a5b39b63f9f0643c8ddd6ee638ac514653
Manuel Spagnolo <spagnolo.manu@gmail.com>
2019-04-13 20:32:32 +0200

    fix: allow scrolling and use offset to get content in the viewport

--- src/templates/Main/Main.js
+++ src/templates/Main/Main.js
@@ -1,9 +1,22 @@
 import styled from "styled-components";
 
-import { headerHeight } from "./Header";
+import {Size} from "~/theme";
 
-export const Main = styled.main(({ theme }) => `
+import {headerHeight} from "./Header";
+
+const scrollOffset = new Size(1);
+
+export const Main = styled.main(({theme}) => `
   padding-top: ${headerHeight};
   max-width: ${theme.breakpoint.sm};
   margin: auto;
+  
+  h1:target[id]:before,
+  h2:target[id]:before,
+  h3:target[id]:before {
+    content:"";
+    display: block;
+    height: ${headerHeight.add(scrollOffset)};
+    margin: ${headerHeight.add(scrollOffset).multiply(-1)} 0 0;
+  }
 `);
