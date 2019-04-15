55cab9f08e26ad901547c172c4ddc281df9bf9d7
Manuel Spagnolo <spagnolo.manu@gmail.com>
2019-04-15 23:40:10 +0200

    feat: use gatsby-remark-gist

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,7 @@ module.exports = {
               wrapperStyle: "margin-bottom: 1.0725rem"
             }
           },
+          "gatsby-remark-embed-gist",
           "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
           "gatsby-remark-smartypants",
