de987b3065830beb508d0f3e6651e02435b99ab3
shikaan <spagnolo.manu@gmail.com>
2019-07-09 09:02:16 +0200

    fix: update base url in constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const REPO_BASE_URL = "https://github.com/shikaan/cdd";
+export const REPO_BASE_URL = "https://github.com/shikaan/full-stack-with-benefits";
 export const NEWSLETTER_WEBHOOK_ADDRESS = "https://hooks.zapier.com/hooks/catch/4102127/pwnf2a/silent/";
