299fcb1171e71e1f719b74a9cc7c5731ca37b272
Manuel Spagnolo <spagnolo.manu@gmail.com>
2020-08-09 15:29:59 +0200

    fix: update gist plugin to include theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,14 @@ module.exports = {
               wrapperStyle: "margin-bottom: 1.0725rem"
             }
           },
-          "gatsby-remark-embed-gist",
+          {
+            resolve: "gatsby-remark-embed-gist",
+            options: {
+              gistDefaultCssInclude: true,
+              gistCssPreload: false,
+              gistCssUrlAddress: "https://github.githubassets.com/assets/gist-embed-b3b573358bfc66d89e1e95dbf8319c09.css"
+            }
+          },
           "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
           "gatsby-remark-smartypants",
